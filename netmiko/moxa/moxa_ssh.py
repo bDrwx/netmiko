75059bdf6c6727325666f3cd81749882238442c9
bodosovaa <bodosovaa@stn.transneft.ru>
2021-04-22 12:19:26 +0300

    Add session preparation for moxa

--- netmiko/moxa/moxa_ssh.py
+++ netmiko/moxa/moxa_ssh.py
@@ -1,5 +1,15 @@
 from netmiko.cisco_base_connection import CiscoSSHConnection
 
 
-class MoxaSSH(CiscoSSHConnection):
+class MoxaEDSBase(CiscoSSHConnection):
+    """ Support Moxa EDS/IKS device series"""
+    def sessionn_preparation(self):
+        """Prepare the session after the connection has been established"""
+        self._test_channel_read(pattern=r"#")
+        self.set_base_prompt()
+        time.sleep(0.3 * self.global_delay_factor)
+        self.clear_buffer()
+
+
+class MoxaSSH(MoxaEDSBase):
     pass
